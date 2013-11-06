c79525f358d7cd558393f4e34eee4b33d26633f4
Steven <steven.ferguson11@gmail.com>
2013-11-06 09:51:43 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AddressBook::Application.config.secret_key_base = '219092ee02dc4d2238b60050148025f36a3cc031db1950c50b72012fa12eebcb47dcba385759980aa8c8fac54286a6889a1a6fe1ce01e692015b4a6320879885'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AddressBook::Application.config.session_store :cookie_store, key: '_address-book_session'
