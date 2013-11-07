a057611c4440a0b8afe6b30e004d767f15f818f7
Steven <steven.ferguson11@gmail.com>
2013-11-07 13:06:45 -0800

    adds ability to create new contacts

---
+++ app/assets/javascripts/controllers/contacts_new_controller.js
@@ -0,0 +1,12 @@
+AddressBook.ContactsNewController = Ember.Controller.extend({
+  actions: {
+    createContact: function() {
+      var name = this.get('newName');
+      if (!name.trim()) { return; }
+      var contact = this.store.createRecord('contact', {
+        name: name
+      });
+      contact.save();
+    }
+  }
+});
