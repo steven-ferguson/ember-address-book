a3b0177ea129d23718e962e80c645bc3612e88c7
Steven <steven.ferguson11@gmail.com>
2013-11-07 12:12:37 -0800

    contacts route shows all contacts in side nav

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,6 +1,13 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 AddressBook.Router.map(function() {
-
+  this.resource('contacts', function() {
+    this.route('new');
+  });
 });
 
+AddressBook.ContactsRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.findAll('contact');
+  }
+});
